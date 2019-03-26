0cff771d47414ea9c1e07440ce59954c52df2612
dittmanns <dittmanns@ergovia.de>
2019-03-26 14:10:49 +0100

    User as objects and check with array.map

--- login.js
+++ login.js
@@ -1,17 +1,31 @@
 'use strict';
 
-const DB_MEMBER = {username: 'stefan', password: '123'};
+const DB_MEMBER = [
+                {username: 'tom', password: '456'}, 
+                {username: 'stefan', password: '123'},
+                {username: 'josef', password: 'qwertz'}
+            ];
 
 const USER = document.querySelector('#login .username');
 const PASSWORD = document.querySelector('#login .password');
 const SUBMIT = document.querySelector('input.submit');
 
 let login = function() {
-    if(DB_MEMBER.username == USER.value && DB_MEMBER.password == PASSWORD.value) {
-        window.location.replace('test.html');
-    }else { 
+    let isUser = DB_MEMBER.map(function(e) { 
+        return e.username; 
+    }).indexOf(USER.value);
+
+    if(isUser >= 0){
+        if(DB_MEMBER[isUser].username == USER.value) {
+            if(DB_MEMBER[isUser].password == PASSWORD.value) {
+                window.location.replace('test.html');
+            }else {
+                console.log('Benutzer nicht bekannt oder Passwort falsch');
+            }
+        }
+    }else {
         console.log('Benutzer nicht bekannt oder Passwort falsch');
-    }    
+    }
 };
 
 SUBMIT.addEventListener('click', function() {login()});
